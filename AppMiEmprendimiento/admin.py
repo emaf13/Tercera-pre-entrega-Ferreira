7dd34172b30670f29d139fc608fd4505795ea2ac
Emanuelle Ferreira <emaf213@gmail.com>
2023-10-05 18:00:55 -0300

    agregamos models y db con datos de prueba

--- AppMiEmprendimiento/admin.py
+++ AppMiEmprendimiento/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from AppMiEmprendimiento.models import Proveedor, Cliente, Pedido, Venta
 
 # Register your models here.
+
+admin.site.register(Proveedor)
+admin.site.register(Cliente)
+admin.site.register(Venta)
+admin.site.register(Pedido)
